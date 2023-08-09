774c2a3779f564302f1deae3c76a59862e426d10
gabeyamartino <gabeyamartino@gmail.com>
2023-08-09 10:57:19 -0400

    Added server directory inside src, got express server up and running

---
+++ src/server/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import path from "path";
+import cors from "cors";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+console.log(__filename);
+console.log(__dirname);
+
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("/", express.static("src/dist"));
+app.use(cors());
+
+app.get("/*", (_req, res) => {
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
