cb428eee8369bbc487262545b267df57bf6e9b91
gabeyamartino <gabeyamartino@gmail.com>
2023-08-17 10:35:11 -0400

    minor cleanup, removed commented out work in seedjs

--- db/seed.js
+++ db/seed.js
@@ -23,36 +23,4 @@ pool.getConnection((err, connection) => {
       }
     });
   }
-
-  // connection.query(statements[0], (err, result) => {
-  //   if (err) {
-  //     console.error("First query error:", err);
-  //     return result;
-  //   }
-
-  //   connection.query(statements[1], (err, result) => {
-  //     if (err) {
-  //       console.error("Second query error:", err);
-  //       return;
-  //     }
-
-  //     connection.query(statements[2], (err, result) => {
-  //       if (err) {
-  //         console.error("Third query error:", err);
-  //         return;
-  //       }
-
-  //       console.log("Created tables in database");
-
-  //       // Release the connection back to the pool
-  //       connection.release();
-
-  //       console.log(
-  //         `Connected to '${connection.config.database}' on port ${connection.config.port}`
-  //       );
-  //       return result;
-  //     });
-  //     return result;
-  //   });
-  // });
 });
