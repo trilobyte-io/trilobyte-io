daee1b193c8a93adf60fbdf90f0fc80ac8edb747
gabeyamartino <gabeyamartino@gmail.com>
2023-08-26 13:58:27 -0400

    Toying with getting temphum data displayed as chart using chartjs

--- src/components/TempHumidityChart.jsx
+++ src/components/TempHumidityChart.jsx
@@ -1,22 +1,53 @@
-import "chart.js";
+// import "chart.js";
 
-const TempHumidityChart = ({ data }) => {
+// const TempHumidityChart = ({ data }) => {
 
-  const cfg = {
-    type: 'line',
-    data: {
-      datasets: [{
-        data: [{x: '2016-12-25', y: 20}, {x: '2016-12-26', y: 10}]
-      }]
-    }
-  }
+//   const cfg = {
+//     type: 'line',
+//     data: {
+//       datasets: [{
+//         data: [{x: '2016-12-25', y: 20}, {x: '2016-12-26', y: 10}]
+//       }]
+//     }
+//   }
 
-console.log(data)
-  return (
-  <div>
-    {data.map(el => el.humidity)}
-  </div>
-  )
+// console.log(data)
+//   return (
+//   <div>
+//     {data.map(el => el.humidity)}
+//   </div>
+//   )
+// }
+
+// export default TempHumidityChart;
+
+import React, { useEffect, useRef } from 'react';
+import {Chart} from 'chart.js';
+
+function TempHumidityChart({ data }) {
+  const chartRef = useRef();
+
+  useEffect(() => {
+    const myChartRef = chartRef.current.getContext('2d');
+
+    new Chart(myChartRef, {
+      type: 'line',
+      data: {
+        labels: data.map(entry => entry.label),
+        datasets: [{
+          label: 'My Line Graph',
+          data: data.map(entry => entry.value),
+          borderColor: 'rgba(75, 192, 192, 1)',
+          fill: false,
+        }],
+      },
+      options: {
+        // Customize options as needed
+      }
+    });
+  }, [data]);
+
+  return <canvas ref={chartRef} />;
 }
 
 export default TempHumidityChart;
