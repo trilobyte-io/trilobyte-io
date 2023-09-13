c9cf473f96185184b66b5896afb225bc2165f691
gabeyamartino <gabeyamartino@gmail.com>
2023-09-13 13:55:27 -0400

    syntax fix for post query

--- server/controllers/postData.js
+++ server/controllers/postData.js
@@ -2,12 +2,12 @@ import pool from "../../db/connect.js"
 
 const postData = (request, response) => {
   let query = `INSERT INTO tempHum
-                VALUES (17,2023-09-13 08:36:21,10,15)`;
+                VALUES (17,2023-09-13 08:36:21,10,15);`;
 
   pool.query(query, (error, results) => {
     if (error) {
       console.log("ERROR IN CONTROLLER: ", error);
-      response.status(500).json({ error: 'An error occurred while fetching data.' });
+      response.status(500).json({ error: 'An error occurred while posting data.' });
     } else {
       response.status(200).json(results);
     }
