e76faa466c18e6a3a178ed01efb38e075d7cfdb7
gabeyamartino <gabeyamartino@gmail.com>
2023-09-13 13:02:46 -0400

    added streamfactory method onto connection config

--- db/connect.js
+++ db/connect.js
@@ -3,12 +3,15 @@ import { config } from "dotenv";
 
 config(); // Load environment variables from .env
 
+const streamFactory = () => fs.createReadStream('/Users/gabeyamartino/desktop/repos/trilobyte-io/exampleData/tempHumidity.csv');
+
 const pool = mysql.createPool({
   user: process.env.user,
   host: process.env.host,
   database: process.env.database,
   password: process.env.password,
   port: process.env.port,
+  streamFactory: streamFactory
 });
 
 export default pool;
