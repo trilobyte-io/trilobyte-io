e6f82af4133614006200c76f3a4cd0ed355f68ed
gabeyamartino <gabeyamartino@gmail.com>
2023-09-13 22:31:52 -0400

    Adding a vite vlose plugin to stop the hanging on build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import ClosePlugin from './vite-plugin-close.js';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ClosePlugin()],
   root: "src",
   build: {
     watch: false
