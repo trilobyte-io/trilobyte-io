0b1652787f4226f0575a3933deb7c96eaa1e7ef4
gabeyamartino <gabeyamartino@gmail.com>
2023-09-21 13:00:09 -0400

    handling label crowding

--- src/components/LuxChart.jsx
+++ src/components/LuxChart.jsx
@@ -47,6 +47,9 @@ const LuxChart = ({ data, timeRange }) => {
             ticks: {
               callback: function(val, index) {
                 // Hide every 2nd tick label
+                if (timeRange === "pastDay") {
+                  return index % 10 === 0 ? this.getLabelForValue(val) : '';
+                }
                 return index % 4 === 0 ? this.getLabelForValue(val) : '';
               },
             }
