63b4642707a937a6b8230553619b0150f24eead7
gabeyamartino <gabeyamartino@gmail.com>
2023-10-12 16:00:16 -0400

    Removed reactgaugechart, looking for alternatives

--- src/components/RealTimeGauge.jsx
+++ src/components/RealTimeGauge.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import GaugeChart from 'react-gauge-chart';
+// import GaugeChart from 'react-gauge-chart';
 
 
 const RealTimeGauge = () => {
 
-  return <GaugeChart id="gauge-chart6"
-  animate={false}
-  nrOfLevels={15}
-  percent={0.56}
-  needleColor="#345243"
-/>
+return (
+  <div></div>
+)
 };
 
 export default RealTimeGauge;
