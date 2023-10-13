fb36f703b8ecfd90aba3ba571cd834095d958254
gabeyamartino <gabeyamartino@gmail.com>
2023-10-13 19:04:02 -0400

    fix

--- server/controllers/postSensorData.js
+++ server/controllers/postSensorData.js
@@ -3,10 +3,9 @@ import pool from "../../db/connect.js"
 const postSensorData = (request, response) => {
   let data = request.body;
   const currentDate = new Date();
-  console.log(currentDate)
+  console.log( request.body)
 
   const mysqlDateTime = currentDate.toISOString().slice(0, 19).replace('T', ' ');
-
 
   let query = `INSERT INTO tempHum (time, temperature, humidity, lux)
                VALUES ('${mysqlDateTime}', ${data.SHT_T}, ${data.SHT_RH}, ${data.TSL_lux});`;
@@ -18,7 +17,8 @@ const postSensorData = (request, response) => {
       console.log("ERROR IN POST CONTROLLER: ", error);
       response.status(500).json({ error: 'An error occurred while posting data.' });
     } else {
-      response.status(200).json("success");
+      // End the response here after successful database insertion
+      response.status(200).json("success").end();
     }
   });
 }
